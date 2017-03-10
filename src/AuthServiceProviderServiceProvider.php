8435f3825a444eaf948acef7ea29c80d66d7fb74
Niklas Grau <niklas.grau@plentymarkets.com>
2017-03-10 09:33:21 +0100

    CHANGE renamed plugin to EbayOAuthTwo

---
+++ src/AuthServiceProviderServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace EbayOAuth2;
+
+use Plenty\Plugin\ServiceProvider;
+
+/**
+ * Class AuthServiceProvider
+ */
+class AuthServiceProvider extends ServiceProvider
+{
+	/**
+	 * @return void
+	 */
+	public function register()
+	{
+	}
+}
